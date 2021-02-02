9118615c6a75c345f8f9931e7422a0eb4a4c131f
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2021-02-02 09:51:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ window.onload=function(){
 	var inp = document.getElementsByClassName("inp");
 	for(let i=0; i<2;i++){
 		inp[i].addEventListener("input", function(){
-			console.log(this);
 			if(this.value != ""){
 				this.classList.add("active")
 			}else this.classList.remove("active");
 		})
 	}
+	inp[1].addEventListener("keydown", function(k){
+		if(k.key == "Enter") login.click();
+	})
 	var show = document.getElementById("show");
 	show.onclick = function(){
 		if(pass.getAttribute("type") == "password"){
@@ -24,14 +26,31 @@ window.onload=function(){
 		}
 	}
 	var url = "https://script.google.com/macros/s/AKfycbzz3LNrwY2k2WKzDOsUizxiWFC_B21T4Pc2lpFdNB3glMsoKkvEOUPy/exec";
+	var clicked = false;
 	login.addEventListener("click",function(){
+		if(!clicked){
+			document.querySelector(".contain-slide").style.display = "none";
+			this.classList.add("clicked");
+			this.innerText = "Processing...";
+			checkLogin(this);
+		}
+		clicked = true;
+	})
+	function checkLogin(ptr){
 		let request = new XMLHttpRequest();
 		request.open("GET",url + "?username=" + username.value + "&password=" + password.value);
 		request.onreadystatechange = function(){
 			if(this.readyState == 4){
-				alert(this.responseText)
+				clicked = false;
+				ptr.innerText = "Login";
+				ptr.classList.remove("clicked");
+				if(this.responseText == "false"){
+					document.querySelector(".contain-slide").style.display = "block";
+				}else{
+					alert("Login Successfully!")
+				}
 			}
 		}
 		request.send()
-	})
+	}
 }
